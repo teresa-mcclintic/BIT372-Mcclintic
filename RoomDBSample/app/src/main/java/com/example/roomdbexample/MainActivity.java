88010b4573e93ed3dfbafa73f420b8805c9f1bd2
Teresa McClintic <tmcclintic@gmail.com>
2021-01-16 18:38:46 -0800

    "removed FAB"

--- RoomDBSample/app/src/main/java/com/example/roomdbexample/MainActivity.java
+++ RoomDBSample/app/src/main/java/com/example/roomdbexample/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.roomdbexample.roomdb.Event;
 import com.example.roomdbexample.roomdb.EventDao;
@@ -27,10 +33,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, CreateEventActivity.class);
-        FloatingActionButton fab = findViewById(R.id.add_event_btn);
-        fab.setOnClickListener((view) -> startActivity(intent));
+        /*FloatingActionButton fab = findViewById(R.id.add_event_btn);
+        fab.setOnClickListener((view) -> startActivity(intent));*/
         eventList = findViewById(R.id.event_list);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Toast toast = Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT);
+        toast.show();
+
+        super.onOptionsItemSelected(item);
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        startActivity(intent);
+        return true;
+
+
+        }
+
+
 
     @Override
     protected void onResume() {
